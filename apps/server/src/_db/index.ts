07ef4969b5e97eadeafb16beaf3bad1512565a3e
hacxy <hacxy.js@outlook.com>
2026-04-22 12:50:24 +0800

    refactor(server): 移除 Prisma ORM 相关代码与配置

--- apps/server/src/_db/index.ts
+++
@@ -1,5 +0,0 @@
-import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3'
-import { PrismaClient } from '@prisma/client'
-
-const adapter = new PrismaBetterSqlite3({ url: 'file:./sqlite.db' })
-export const db = new PrismaClient({ adapter })
